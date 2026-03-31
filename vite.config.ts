110173c88707c1d3a84b2d7f7ca972b8e84a4ad1
Lanka Saran Manikanta swamy <saranlanka033@gmail.com>
2026-03-31 12:55:56 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/code-companion/',
+  base: '/prank-result-studio/',
 })
